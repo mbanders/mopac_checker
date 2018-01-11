e5548e2d02ee7227bd0a5a6b19ab6e34bada3f06
Mike Anderson <michael.anderson@arm.com>
2018-01-11 10:51:58 -0600

    Add user agents, CSV function
    Signed-off-by: Mike Anderson <michael.anderson@arm.com>

--- mopac_check.py
+++ mopac_check.py
@@ -19,7 +19,9 @@ def get_mopac_data(when):
     url = 'https://mopac-fare.mroms.us/HistoricalFare/ViewHistoricalFare'
     USER_AGENTS = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7',
-                   'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_2 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C202 Safari/604.1']
+                   'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_2 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0 Mobile/15C202 Safari/604.1',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko']
     headers = {
         'Origin': 'https://www.mobilityauthority.com',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -53,9 +55,22 @@ def randsleep(seconds=0):
     time.sleep(random.randint(0,seconds))
     return None
 
+def append_csv(data, name='result.csv'):
+    keys = ['date','time','day_of_week',
+            'NB: 2222 to Parmer','NB: CVZ to 183','NB: CVZ to Parmer',
+            'SB: 2222 to 5th/CVZ','SB: Parmer to 2222','SB: Parmer to 5th/CVZ']
+    with open(name, "a") as f:
+        line = []
+        for k in keys:
+            line.append(str(data[k]))
+        f.write(",".join(line) + "\n")
+    print("Wrote to %s" % name)
+    return None
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("sec", help="Randomly sleep between 0 and this many seconds", type=int, default=0)
+    parser.add_argument("-s", "--sec", help="Randomly sleep between 0 and this many seconds", type=int, default=0)
+    parser.add_argument("-o", "--out", help="Append data to this csv file", type=str, default="result.csv")
     args = parser.parse_args()
     randsleep(args.sec)
     now = datetime.datetime.now()
@@ -68,4 +83,5 @@ if __name__ == '__main__':
     nice_data['date'] = now.strftime("%Y-%m-%d")
     nice_data['time'] = now.strftime("%H:%M")
     nice_data['day_of_week'] = now.strftime("%a")
+    append_csv(nice_data, args.out)
     printd(nice_data)
