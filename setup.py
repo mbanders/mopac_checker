35f34fa4b01b01554aca1873b8ff12097af794d6
Mike Anderson <mike@meldnetworks.com>
2020-07-23 13:50:00 -0500

    Add long description to setup.py
    Signed-off-by: Mike Anderson <mike@meldnetworks.com>

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@
 
 from setuptools import find_packages, setup
 
+# read Readme file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="mopac",
-    version="0.9",
+    version="0.9.1",
     python_requires='>3.5.0',
     description="Check Mopac Fast Lane Prices",
     author="Michael Anderson",
     url="https://github.com/mbanders/mopac_checker",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     package_data={"": ["*.txt", "*.json", "*.cfg", "*.md"]},
     include_package_data=True,
